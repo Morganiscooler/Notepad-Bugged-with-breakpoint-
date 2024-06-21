209aab0e3e6ad2ac46ddd4882584ca21feb155dd
Morgan C <morgancuiuwu@gmail.com>
2024-06-21 16:25:33 -0400

    Figured out the issue: Items don't naturally contain a string

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
 
 namespace Notepad__easy_.ViewModel
 {
@@ -10,6 +11,7 @@ namespace Notepad__easy_.ViewModel
         // The ID system is not fully developed yet...
         public MainViewModel()
         {
+
             Items = new ObservableCollection<string>();
 
         }
@@ -33,7 +35,7 @@ namespace Notepad__easy_.ViewModel
 
 
         [ICommand]
-        private async void Add()
+        async Task Add()
         {
             //put this outside the func later
 
@@ -50,19 +52,26 @@ namespace Notepad__easy_.ViewModel
                 Items.Add(Text);
             }
             // Add a function here that removes the "Let's get started!" button after the first new note has been added
-            Text = string.Empty;
+            //Text = string.Empty;
 
             await Shell.Current.GoToAsync($"../../");
         }
 
         [ICommand]
-        void Delete(string s)
+        void Delete()
         {
-            // Fix the delete command not working
-            if (Items.Contains(s))
+            string s = Text;
+            // Problem: Item does not contain s, find what else it contains so it can remove it.
+            // items will never contain Text, because it gets reset to string.Empty at the end.
+
+            // Find a way to remove anything that contains ANY string and not just a specific set of strings
+            if (!Items.Contains(s))
             {
-                Console.WriteLine($"{s} has been deleted!");
-                Items.Remove(s);
+                Items.Add(s);
+            }
+            else 
+            {
+                Items.Remove(s); 
             }
             //removes the item from the MainPage's viewmodel
         }
@@ -84,9 +93,15 @@ namespace Notepad__easy_.ViewModel
         async Task Tap(string s) 
         {
             string n = Note;
-            Changecolor = BackgroundColor;
-            Color color = Changecolor;
+            // the observableproperty Changecolor is NULL fix this ASAP or else the color won't bind to ANY PAGE!!
+            //Changecolor = BackgroundColor;
+
+            //Color color = Changecolor;
             // Need to modify NotePage so it saves a different text for each individual newly created note
+
+            // Changecolor is not working ?
+            Changecolor = Colors.Red; // Change this to be flexible to the 6 different colors + choose from file
+            Color color = Changecolor;
             await Shell.Current.GoToAsync($"{nameof(NewNotePage)}?Text={s}&Note={n}&Color={color}");
         }
 
@@ -114,6 +129,7 @@ namespace Notepad__easy_.ViewModel
 
                     //Change here button background colors
                     BackgroundColor = Color.FromHex("ffe66e"); //or something
+                    
                 });
             }
         }
@@ -126,6 +142,7 @@ namespace Notepad__easy_.ViewModel
 
                     //Change here button background colors
                     BackgroundColor = Color.FromHex("a1ef9b"); //or something
+                    Changecolor = Color.FromHex("a1ef9b"); // This works when inside this command, however I am unsure how to pass it through the QueryProperty.
                 });
             }
         }
